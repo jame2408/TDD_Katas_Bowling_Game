c5cc3f3276c8002a052aca84e5fd244dc5fe73d1
jame2408 <jame2408@gmail.com>
2017-03-12 20:29:50 +0800

    依據第二個測試個案，撰寫程式碼並通過測試。（綠燈）

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -4,7 +4,9 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int _score;
+        private int[] _rolls = new int[21];
+        private int _currentRoll = 0;
+
 
         public Game()
         {
@@ -12,12 +14,27 @@ namespace BowlingGame
 
         public void roll(int pins)
         {
-            this._score += pins;
+            _rolls[_currentRoll++] = pins;
         }
 
         public int score()
         {
-            return _score;
+            int score = 0;
+            var i = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[frame] + _rolls[frame + 1] == 10)
+                {
+                    score += 10 + _rolls[i + 2];
+                    i += 2;
+                }
+                else
+                {
+                    score += _rolls[i] + _rolls[i + 1];
+                    i += 2;
+                }
+            }
+            return score;
         }
     }
 }
