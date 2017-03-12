8ee555f653faf75710066b623e272af20c67f476
jame2408 <jame2408@gmail.com>
2017-03-12 20:03:22 +0800

    重構程式碼，因確定目前 feature 為每次投球分數之總和，故利用重構時撰寫完程式並通過測試。（綠燈）

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -4,18 +4,20 @@ namespace BowlingGame
 {
     public class Game
     {
+        private int _score;
+
         public Game()
         {
         }
 
         public void roll(int pins)
         {
-            
+            this._score += pins;
         }
 
         public int score()
         {
-            return 0;
+            return _score;
         }
     }
 }
