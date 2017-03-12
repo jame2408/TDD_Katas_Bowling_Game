c6a0d34e8486775fb8304919b19bb99183bcc098
jame2408 <jame2408@gmail.com>
2017-03-12 20:13:32 +0800

    撰寫第二個測試個案，想驗證有一個 Spare(補中) 下，其計算分數是否正確。（紅燈）

--- BowlingGameTest/BowlingGameTest.cs
+++ BowlingGameTest/BowlingGameTest.cs
@@ -19,5 +19,22 @@ namespace BowlingGameTest
 
             Assert.AreEqual(0, game.score());
         }
+
+        [TestMethod]
+        public void testOneSpare()
+        {
+            var game = new Game();
+            game.roll(5);
+            game.roll(5); // spare
+            game.roll(3);
+
+            for (int i = 0; i < 17; i++)
+            {
+                game.roll(0);
+            }
+
+            Assert.AreEqual(16, game.score());
+        }
+
     }
 }
